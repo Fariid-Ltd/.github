e4a0236e98de93f89a51671c65ca79dd600927a0
Moha Abdi <83307623+moha-abdi@users.noreply.github.com>
2024-10-19 17:00:55 +0300

    Update update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -5,18 +5,22 @@ from datetime import datetime, timezone
 # Initialize GitHub client
 g = Github(os.environ['GITHUB_TOKEN'])
 
-# Get the user (your personal account)
 user = g.get_user("moha-abdi")
 
-# Specify the repositories we want to include
-repo_names = ["Sarifle", "SIPUserRegistrar", "TronWatch", "baileys-api"]
+repo_names_branches = {
+    "Sarifle": "fariid-rebrand",
+    "SIPUserRegistrar": "main",
+    "TronWatch": "main",
+    "baileys-api": "main"
+}
 
 # Prepare the repository information
 repo_info = []
-for name in repo_names:
+for name, branch in repo_names_branches.items():
     try:
         repo = user.get_repo(name)
-        last_updated = repo.updated_at.astimezone(timezone.utc)
+        branch_info = repo.get_branch(branch)
+        last_updated = branch_info.updated_at.astimezone(timezone.utc)
         time_diff = datetime.now(timezone.utc) - last_updated
         
         if time_diff.days == 0:
@@ -34,16 +38,17 @@ for name in repo_names:
             update_text = f"{time_diff.days // 7} weeks ago"
         else:
             update_text = f"{time_diff.days // 30} months ago"
-        
+
+        repo_name = "Fariid" if branch_info.name == "Sarifle" else branch_info.name
         repo_info.append({
-            'name': repo.name,
-            'description': repo.description or "*No description provided*",
-            'language': repo.language,
-            'private': repo.private,
-            'fork': repo.fork,
+            'name': branch_info.name,
+            'description': branch_info.description or "*No description provided*",
+            'language': branch_info.language,
+            'private': branch_info.private,
+            'fork': branch_info.fork,
             'updated': update_text,
-            'license': repo.license.name if repo.license else "No license",
-            'topics': repo.get_topics()
+            'license': branch_info.license.name if branch_info.license else "No license",
+            'topics': branch_info.get_topics()
         })
     except Exception as e:
         print(f"Error fetching repository {name}: {str(e)}")
@@ -55,9 +60,10 @@ with open('README_template.md', 'r') as file:
 # Generate the repository list
 repo_list = ""
 for repo in repo_info:
+    repo_url = f"https://github.com/moha-abdi/{repo['name']}" if repo['name'] != "Fariid" else "#"
     privacy = "🔒 Private" if repo['private'] else "🌐 Public"
     fork_text = " (Fork)" if repo['fork'] else ""
-    repo_list += f"### [{repo['name']}](https://github.com/moha-abdi/{repo['name']}) ({privacy}{fork_text})\n"
+    repo_list += f"### [{repo['name']}](repo_url) ({privacy}{fork_text})\n"
     repo_list += f"- **Description**: {repo['description']}\n"
     repo_list += f"- **Language**: {repo['language']}\n"
     if repo['topics']:
